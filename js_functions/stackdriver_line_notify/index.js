e0051c93c4959b1fce2daea12b1b3d3e74995a61
terukusu <teru.kusu@gmail.com>
2019-02-17 22:49:50 +0900

    監視ツールであるStackdriverから何かあったときに通知を受け取り、Lineへ通知するCloud Functionsを追加。

---
+++ js_functions/stackdriver_line_notify/index.js
@@ -0,0 +1,67 @@
+const request = require('request');
+
+auth = require('./auth.json');
+config = require('./config.json');
+
+exports.stackdriverLineNotify = (req, res) => {
+  let auth_token = req.query.auth_token;
+
+  if (!auth_token || auth_token !== auth.auth_token) {
+    res.status(403).send('Forbidden');
+  }
+
+  // Stackdriverからのメッセージ
+  let sd_json = req.body;
+  let i = sd_json.incident;
+
+  let message = '【SVR1】Stackdriverからのお知らせでです。\n'
+    + '状況: ' + i.state + '\n'
+    + '対象: ' + i.resource_name + '\n'
+    + 'ポリシー: ' + i.policy_name + '\n'
+    + '発生時刻: ' + getJstMmddhhmm(i.started_at) + '\n'
+    + '収束時刻: ' + getJstMmddhhmm(i.ended_at) + '\n'
+    + 'サマリ: ' + i.summary + '\n'
+    + '詳細: ' + i.url;
+
+  var options = {
+    uri: config.line_push_url,
+    headers: {
+      'Content-type': 'application/json; charset=utf-8',
+      'Authorization': 'Bearer ' + auth.line_token
+    },
+    json: {
+      'to': auth.line_user_id,
+      "messages": [{
+        'type': 'text',
+        'text': message
+      }]
+    }
+  };
+
+  // Line へメッセージ送信！
+  request.post(options, (e, r, b) => {
+    if (e || r.statusCode != 200) {
+      console.log(e + r + b);
+    }
+  });
+
+  res.status(200).send('OK');
+};
+
+function getJstMmddhhmm(unixtime) {
+
+  if (!unixtime) {
+    return '';
+  }
+
+  let d = new Date((unixtime + (3600 * 9)) * 1000);
+
+  let month = d.getUTCMonth() + 1;
+  let date = d.getUTCDate();
+  let hour = d.getUTCHours();
+  let minute = d.getUTCMinutes();
+
+  let date_str = month + '/' + date + ' ' + hour + ':' + minute;
+
+  return date_str;
+}
